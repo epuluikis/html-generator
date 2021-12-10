83942a403e734caa9e9ddb2f827852ae4601a9b7
jhh8 <95616101+jhh8@users.noreply.github.com>
2021-12-10 16:44:28 +0200

    add data storing into a structure
    + a simple interface

--- main.c
+++ main.c
@@ -5,6 +5,102 @@
 #include <fileapi.h>
 #include "resources/template.h"
 
+#define STR_MAX_LENGTH 256
+// amount of variables
+#define FIELD_COUNT 14
+#define SECTION_COUNT 5
+#define SECTION_MAX_SIZE 7
+
+struct Contents {
+	
+	char titles_tochange[FIELD_COUNT][STR_MAX_LENGTH];		// text to replace in the html template
+	char interface_text[FIELD_COUNT][STR_MAX_LENGTH];		// text to show in the interface
+	char section_titles[SECTION_COUNT][STR_MAX_LENGTH];		// names of different sections
+	int section_ids[SECTION_COUNT][SECTION_MAX_SIZE];		// store which section replaces which titles
+	char user_input[FIELD_COUNT][STR_MAX_LENGTH];			// currently now used as we are not storing user input but writing it into HTML right away
+	
+}; 
+
+void InitStructure(struct Contents *contents)
+{
+	const char title_names[FIELD_COUNT][STR_MAX_LENGTH] =
+	{
+		"{{user_name}}",
+		"{{description}}",
+		"{{title}}",
+		"{{text}}",
+		"{{subtitle}}",
+		"{{phone_title}}",
+		"{{phone}}",
+		"{{email_title}}",
+		"{{email}}",
+		"{{address_title}}",
+		"{{address}}",
+		"{{image}}",
+		"{{alt}}",
+		"{{current_year}}"
+	};
+	
+	const char interface_text_names[FIELD_COUNT][STR_MAX_LENGTH] =
+	{
+		"User Name",		// 0
+		"Description",		// 1
+		"Title",			// 2
+		"Text",				// 3
+		"Subtitle",			// 4
+		"Phone Title",		// 5
+		"Phone Number",		// 6
+		"Email Title",		// 7
+		"Email",			// 8
+		"Address Title",	// 9
+		"Address",			// 10
+		"Image",			// 11
+		"Alt",				// 12
+		"Current Year"		// 13
+	};
+	
+	const char section_title_names[SECTION_COUNT][STR_MAX_LENGTH] =
+	{
+		"General Section",	// 0
+		"Text + Image",		// 1
+		"Hero Section",		// 2
+		"Contacts Section",	// 3
+		"Carousel Section"	// 4
+	};
+	
+	// -1 indicates stop reading ids
+	const int section_ids[SECTION_COUNT][SECTION_MAX_SIZE + 1] =
+	{
+		{ 0, 1, 2, -1 },	// General Section	
+		{ 2, 3, -1 },		// Text + Image
+		{ 2, 4, -1 },		// Hero Section	
+		{ 2, 5, 6, 7, 8, 9, 10, -1 },	// Contacts Section	
+		{ 0, 11, 12, -1 },	// Carousel Section	
+	};
+	
+	// initialize titles_tochange and interface_text
+	for (int i = 0; i < FIELD_COUNT; ++i)
+	{
+		strcpy( contents->titles_tochange[i], title_names[i] );
+		strcpy( contents->interface_text[i], interface_text_names[i] );
+	}
+	
+	// initialize section_titles and section_ids
+	for (int i = 0; i < SECTION_COUNT; ++i)
+	{
+		strcpy( contents->section_titles[i], section_title_names[i] );
+		
+		for (int j = 0; j < SECTION_MAX_SIZE; ++j)
+		{
+			contents->section_ids[i][j] = section_ids[i][j];
+		}
+	}
+	
+
+	
+	//strcpy( contents->titles_tochange[0], "{{user_name}}" );
+}
+
 char *str_replace(char *orig, char *rep, char *with)
 {
     char *result;  // the return string
@@ -83,27 +179,57 @@ int copy_dependencies()
     return 1;
 }
 
-void test_str_replace()
+void writeHTML()
 {
-    char name[] = "{{user_name}}", with[] = "Test User";
-    FILE *test_file;
-
-    strcpy(test_template, str_replace(test_template, name, with));
+	FILE *output_file;
 
     create_output_directories();
 
-    test_file = fopen("./output/index.html", "w");
+    output_file = fopen("./output/index.html", "w");
 
-    fprintf(test_file, "%s", test_template);
+    fprintf(output_file, "%s", test_template);
 
-    fclose(test_file);
+    fclose(output_file);
 
     copy_dependencies();
 }
 
+void test_str_replace()
+{
+    char name[] = "{{user_name}}", with[] = "Test User";
+
+    strcpy(test_template, str_replace(test_template, name, with));
+
+    writeHTML();
+}
+
 int main()
 {
-    test_str_replace();
+    //test_str_replace();
+    
+    struct Contents contents;
+    
+    InitStructure(&contents);
+	
+	for (int i = 0; i < SECTION_COUNT; ++i)
+	{
+		printf("%s: \n", contents.section_titles[i]);
+		int j = 0;
+		while ( contents.section_ids[i][j] >= 0 )
+		{
+			char user_input[256];
+			
+			printf("Enter a %s: ", contents.interface_text[ contents.section_ids[i][j] ]);
+			scanf("%s", user_input);
+			
+			strcpy( test_template, strcpy( test_template, str_replace(test_template, contents.titles_tochange[ contents.section_ids[i][j] ], user_input) ) );
+			++j;
+		}
+
+		printf("\n");
+	}
+	
+	writeHTML();
 
     return 0;
 }
